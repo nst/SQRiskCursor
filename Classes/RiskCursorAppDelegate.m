f60d7711356dce1b8e1bdcbaf8012af06debb887
Nicolas Seriot <nicolas.seriot@swissquote.ch>
2018-02-06 10:27:15 +0100

    runs on iOS 10

--- Classes/RiskCursorAppDelegate.m
+++ Classes/RiskCursorAppDelegate.m
@@ -24,7 +24,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
     
     // Override point for customization after app launch. 
-    [self.window addSubview:viewController.view];
+    [self.window setRootViewController:viewController];
+
     [self.window makeKeyAndVisible];
 
 	return YES;
